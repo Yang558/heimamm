892572137c5736be4d6bc6e8ef1e0c6642e4d95f
Yang558 <979048842@qq.com>
2020-03-15 15:31:39 +0800

    项目基础引入

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
+// 导入 vue
 import Vue from 'vue'
+// 导入 App.vue
 import App from './App.vue'
-
+// 导入 router
+import router from './router'
+//导入element-ui
+import ElementUI from 'element-ui';
+//导入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css';
+//使用element-ui
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
+//创建一个新的 vue 实例
 new Vue({
+  router: router, //将路由对象挂载
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+//导入 Vue
+import Vue from 'vue'
+//导入 vue-router
+import VueRouter from 'vue-router'
+//使用vue-router
+Vue.use(VueRouter)
+// 创建一个新的 vue-router 对象
+var router = new VueRouter({
+    //添加路由的规则
+    routes: [
+
+    ]
+})
+
+//将新的对象暴露出去
+export default router
